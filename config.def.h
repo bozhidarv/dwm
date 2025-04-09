16e697a5ac9d58742c38fb586ce0cd4789329992
Bozhidar Videnov <bozh.bv03@gmail.com>
2025-04-09 12:08:15 +0300

    feat: applied restoreafterrestart patch
    Patch page: https://dwm.suckless.org/patches/restoreafterrestart
    Patch file: https://dwm.suckless.org/patches/restoreafterrestart/dwm-restoreafterrestart-20220709-d3f93c7.diff

--- config.def.h
+++ config.def.h
@@ -1,5 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
+#define SESSION_FILE "/tmp/dwm-session"
+
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const int startwithgaps[]    = { 0 };	/* 1 means gaps are used by default, this can be customized for each tag */
